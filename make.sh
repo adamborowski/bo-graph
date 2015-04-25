abaf9598ea823eaaf12ba5d5f3a864d072c6fe8b
borovsky <fl.borovsky@gmail.com>
2015-04-25 13:21:56 +0200

    production

--- make.sh
+++ make.sh
@@ -1,5 +1,7 @@
-sencha app build
+sencha app build -c production
 ./sync.sh
 git add .
 git commit -am production
 git push origin
+rm -f build/bograph.zip
+zip build/bograph.zip build/production/bo
